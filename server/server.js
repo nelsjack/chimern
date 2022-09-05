73f11d41c5c2b883d32a7e16e63563e4a364b35b
Jack Nelson <jackrnelson47@gmail.com>
2022-09-05 17:39:02 -0400

    feat: create JSONWebToken verification function

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors")
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("./models/userModel")
 require('dotenv').config();
 
 const app = express();
@@ -15,16 +15,37 @@ app.use(cors({
     origin: '*'
 }))
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+//routes
+app.use('/', require('./routes/register'));
+app.use('/', require('./routes/login'));
+
+mongoose.connect(process.env.MONGO_URI)
 .then((res) => {
     app.listen(3333, () => console.log("Connected to MongoDB"))
 })
 .catch(err => console.log(err))
 
-app.get("/home", (req, res) => {
-    res.json({
-        name: "Bill" 
-    })
+app.get("/getUsername", verifyJWT, (req, res) => {
+    res.json({isLoggedIn: true, username: req.user.username})
 })
 
-//app.listen(3333, () => console.log("Server is up"))
+function verifyJWT(req, res, next) {
+    const token = req.headers["x-access-token"]?.split(" ")[1]
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) { 
+                return res.json({
+                    isLoggedIn: false,
+                    message: "Failed to Authenticate"
+                })
+            }
+                req.user = {};
+                req.user.id = decoded.id;
+                req.user.username = decoded.username
+                next()
+        })
+    } else {
+        res.json({message: "Incorrect Token Given", isLoggedIn: false})
+    }
+}
