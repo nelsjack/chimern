135f948687d432a7bcd0038ea8eb32f97766c77f
Jack Nelson <jackrnelson47@gmail.com>
2022-09-08 15:20:50 -0400

    feat: create hi-fi registraation UI

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,12 +1,21 @@
 import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import "./Register.css"
+import mongoat from "../mongoat.png"
+import lionode from "../lionode.png"
+import dreacton from "../dreacton.png"
 
 function Register() {
+const [usernameFieldValue, setUsernameFieldValue] = useState("")
+const [passwordFieldValue, setPasswordFieldValue] = useState("")
+const [buttonStatus, setButtonStatus] = useState("")
+const [creature, setCreature] = useState("")
 const navigate = useNavigate();
 
 useEffect(() => {
     checkUserAuth();
-}, [])
+    checkUserInput();
+})
 
 async function handleRegister(e) {
     e.preventDefault()
@@ -27,9 +36,24 @@ async function handleRegister(e) {
     .then(data => {
         console.log('registration status: ', data)
         localStorage.setItem("token", data.token)
+        console.log('Registration Successful')
         checkUserAuth();
     })
 }
+
+    function displayFlavorText() {
+        if (creature === "mongoat") return "MONGOAT Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut eleifend arcu, id sagittis massa. Sed eu interdum nibh. Sed leo dui, auctor eget hendrerit non, vestibulum nec arcu."
+        if (creature === "dreacton") return "DREACTON Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut eleifend arcu, id sagittis massa. Sed eu interdum nibh. Sed leo dui, auctor eget hendrerit non, vestibulum nec arcu."
+        if (creature === "lionode") return "LIONODE Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut eleifend arcu, id sagittis massa. Sed eu interdum nibh. Sed leo dui, auctor eget hendrerit non, vestibulum nec arcu."
+    }
+
+    function checkUserInput() {
+        if (usernameFieldValue && passwordFieldValue && creature) {
+            setButtonStatus("is-primary")
+        } else {
+            setButtonStatus("is-disabled")
+        }
+    }
 
     function checkUserAuth() {
         fetch("http://localhost:3333/checkUserAuth", {
@@ -40,20 +64,39 @@ async function handleRegister(e) {
     .then(res => res.json())
     .then(data => {
         if(data.isLoggedIn) {
-            console.log('Registration Successful')
             navigate("/")
         }
     })
 }
 
     return (
-        <form onSubmit={event => handleRegister(event)}>
-            <input required type='username'/>
-            <input required type='password'/>
-            <input type='submit' value='Submit'/>
-            <p>Register</p>
-        </form>
-            
+        <div className="registration-container">
+            <h1 className="title">Welcome to CHIMERN</h1>
+            <p className="prompt">Choose a username and password, and choose your first creature!</p>
+            <div className="creature-btn-container">
+                <button className="nes-btn creature-btn" onClick={() => setCreature("mongoat")}>
+                    <img className="creature-sprite" src={mongoat} alt="mongoat"/>
+                </button>
+                <button className="nes-btn creature-btn" onClick={() => setCreature("dreacton")}>
+                    <img className="creature-sprite" src={dreacton} alt="dreacton"/>
+                </button>
+                <button className="nes-btn creature-btn" onClick={() => setCreature("lionode")}>
+                    <img className="creature-sprite" src={lionode} alt="lionode"/>
+                </button>
+            </div>
+            <p className="flavor-text">{displayFlavorText()}</p>
+            <form onSubmit={event => handleRegister(event)}>
+                <div className="nes-field">
+                    <label className="input-label">Username</label>
+                    <input className={"nes-input"} required type="username" onChange={(e) => {setUsernameFieldValue(e.target.value)}}/>
+                    <label className="input-label">Password</label>
+                    <input className={"nes-input"} required type="password" onChange={(e) => {setPasswordFieldValue(e.target.value)}}/>
+                    <div className="nes-btn-container">
+                        <button className={"nes-btn " + buttonStatus} type="submit">Create Account</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     )
 }
 
